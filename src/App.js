b989e6a4970b9114fe5b16657d33688eef039dab
Aleksey Popov <leks.popow@gmail.com>
2020-10-20 00:20:35 +0300

    first:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="logo.png" alt=""/>
       </header>
+      <div className="container">
+        <nav className="nav">
+          <div>
+            Profile
+          </div>
+          <div>
+            Messages
+          </div>
+          <div>
+            News
+          </div>
+          <div>
+            Music
+          </div>
+          <div>
+            Settings
+          </div>
+        </nav>
+        <div className="content">
+          Контент
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
