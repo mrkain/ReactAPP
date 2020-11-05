7986a2a45812e2fb03388e72ef89bd103cb5c2cd
mrkain <leks.popow@gmail.com>
2020-11-05 16:27:55 +0300

    Добавил hover эффект для dialog айтемов

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 const DialogItem = (props) => {
   return (
       <div className={s.item}>
-        <NavLink activeClassName={s.active} to={"/dialogs/" + props.id}> <img className={s.avatar} src={props.avatar} /> {props.name}</NavLink>
+        <NavLink activeClassName={s.active} to={"/dialogs/" + props.id}> <img className={s.avatar} src={props.avatar} /> <span className={s.name}>{props.name}</span></NavLink>
       </div>
   )
 }
